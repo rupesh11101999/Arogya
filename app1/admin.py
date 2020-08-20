06ffa2d6e9e0eed9e4c940f6b0afb6691a4f160b
rupesh <rupeshyadav11101999@gmail.com>
2020-08-20 22:45:14 +0530

    Adding all my project files

---
+++ app1/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(City)
+admin.site.register(State)
+admin.site.register(Patient)
+admin.site.register(Country)
+admin.site.register(User)
+admin.site.register(Clinic)
+admin.site.register(Doctor)
